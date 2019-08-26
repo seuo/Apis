5f00583e36a8f57eddfca23c10e96b391888ef67
Sam <samrob.nz@gmail.com>
2019-08-26 15:08:13 +1200

    added serverside upload function

--- api-app/server.js
+++ api-app/server.js
@@ -4,7 +4,11 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+var fileUpload = require('express-fileupload');
+
 var Project = require('./project-model');
+// var User = require('./user-model');
+var Type = require('./type-model');
 
 //setup database connection
 var connectionString = 'mongodb://projectAdmin:admin123@cluster0-shard-00-00-mmxiv.gcp.mongodb.net:27017,cluster0-shard-00-01-mmxiv.gcp.mongodb.net:27017,cluster0-shard-00-02-mmxiv.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
@@ -18,71 +22,165 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(fileUpload());
+
 app.use(logger('dev'));
 
+
+app.use(express.static('public'));
+
+router.post('/upload', (req, res)=> {
+
+	var files = Object.values(req.files);
+	var uploadedFile = files[0];
+
+	var newName = Date.now() + uploadedFile.name;
+
+	uploadedFile.mv('public/'+ newName,function(){
+		res.send('bla.jpg uploaded')
+	})
+
+	res.json(files);
+});
 //setup routes
 var router = express.Router();
 
-
+router.get('/testing', (req, res) => {
+  res.send('<h1>Testing is working</h1>')
+})
 
 router.get('/projects', (req, res) => {
-    res.send('<h1>Testing is working! But no connection</h1>')
-  })
-  
-  router.get('/projects', (req, res) => {
-      Project.find()
-      .then((projects) => {
-          return res.json(projects);
-      });
-  
-  })
-  
-  router.get('/projects/:id', (req, res) => {
-  
-      Project.findOne({id:req.params.id})
-      .then((project) => {
-          return res.json(project);
-      });
-  })
-  
-  router.post('/projects', (req, res) => {
-  
-      var project = new Project();
-      project.id = Date.now();
-      
-      var data = req.body;
-      Object.assign(project,data);
-      
-      project.save()
-      .then((project) => {
-            return res.json(project);
-      });
-  });
-  
-  router.delete('/projects/:id', (req, res) => {
-  
-      Project.deleteOne({ id: req.params.id })
-      .then(() => {
-          return res.json('deleted');
-      });
-  });
-  
-  router.put('/projects/:id', (req, res) => {
-  
-      Project.findOne({id:req.params.id})
-      .then((project) => {
-          var data = req.body;
-          Object.assign(project,data);
-          return project.save()	
-      })
-      .then((project) => {
-          return res.json(project);
-      });	
-  
-  });
+
+	Project.find()
+	.then((projects) => {
+	    return res.json(projects);
+	});
+
+})
+
+router.get('/projects/:id', (req, res) => {
+
+	Project.findOne({id:req.params.id})
+	.then((project) => {
+	    return res.json(project);
+	});
+})
+
+router.post('/projects', (req, res) => {
+
+	var project = new Project();
+	project.id = Date.now();
+	
+	var data = req.body;
+	console.log(data);
+	Object.assign(project,data);
+	
+	project.save()
+	.then((project) => {
+	  	return res.json(project);
+	});
+});
+
+router.delete('/projects/:id', (req, res) => {
+
+	Project.deleteOne({ id: req.params.id })
+	.then(() => {
+		return res.json('deleted');
+	});
+});
+
+router.put('/projects/:id', (req, res) => {
+
+	Project.findOne({id:req.params.id})
+	.then((project) => {
+		var data = req.body;
+		Object.assign(project,data);
+		return project.save()	
+	})
+	.then((project) => {
+		return res.json(project);
+	});	
+
+});
+
+router.get('/users', (req, res) => {
+
+	User.find()
+	.then((users) => {
+	    return res.json(users);
+	});
+
+})
+
+router.get('/users/:id', (req, res) => {
+
+
+	User.findOne({id:req.params.id})
+	.then((user) => {
+	    return res.json(user);
+	});
+})
+
+router.get('/users/:id/projects', (req, res) => {
+
+
+	Project.find({user_id:req.params.id})
+	.then((projects) => {
+	    return res.json(projects);
+	});
+
+})
+
+router.post('/users', (req, res) => {
+
+	var user = new User();
+	user.id = Date.now();
+	
+	var data = req.body;
+	Object.assign(user,data);
+	
+	user.save()
+	.then((user) => {
+	  	return res.json(user);
+	});
+});
+
+router.delete('/users/:id', (req, res) => {
+
+	User.deleteOne({ id: req.params.id })
+	.then(() => {
+		return res.json('deleted');
+	});
+});
+
+router.put('/users/:id', (req, res) => {
+
+	User.findOne({id:req.params.id})
+	.then((user) => {
+		var data = req.body;
+		Object.assign(user,data);
+		return user.save()	
+	})
+	.then((user) => {
+		return res.json(user);
+	});	
+
+});
+
+router.get('/types', (req, res) => {
+
+	Type.find()
+	.then((types) => {
+
+	    return res.json(types);
+	});
+
+})
+
+
 
 app.use('/api', router);
 
 // launch our backend into a port
-const apiPort = 4005;
+const apiPort = 3001;
 app.listen(apiPort, () => console.log('Listening on port '+apiPort));
--- api-app/type-model.js
+++ api-app/type-model.js
@@ -1,16 +1,15 @@
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // this will be our data base's data structure 
-var ProjectSchema = new Schema(
+var TypeSchema = new Schema(
   {
     id: Number,
     name: String,
-    description: String,
-    photo:String
   },
   { timestamps: true }
 );
 
 // export the new Schema so we could modify it using Node.js
-module.exports = mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model('Type', TypeSchema);
